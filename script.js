b5a932d07d6fd242e35895c9326f3b3c606a7667
Ele <ejcatalan@hotmail.com>
2022-12-16 10:29:38 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,103 @@
-//display variable to display numbers for the calculator
-let display = document.getElementById('display');
-//array function to create an array for the value of each button
-let buttons = Array.from(document.getElementsByClassName('button'));
+let clear = document.getElementById("clearBtn");
+clear.addEventListener("click", clearCalc);
+function clearCalc() {
+  document.getElementById("results").value = "";
+};
 
+let equal = document.getElementById("equal");
+equal.addEventListener("click", equalBtn);
+function equalBtn() {
+  let sum = document.getElementById("results").value;
+  let answer = eval(sum);
+  document.getElementById("results").value = answer;
+};
 
-//map function to change each button to an array
-buttons.map(button => {
-    button.addEventListener('click',(e) => {
-        switch(e.target.innerText) {
-            case 'C':
-                display.innerText = '';
-                break;
-            case '⇚':
-                if (display.innerText) {
-                    display.innerText = display.innerText.slice(0,1);
-                }
-            case '=':
-                display.innerText = Function('return ' + display.innerText)();
-                break;
-            default:
-                display.innerText += e.target.innerText;
-        }
-    })
-});
+let addition = document.getElementById("addition");
+addition.addEventListener("click", additionBtn);
+function additionBtn() {
+  document.getElementById("results").value += "+";
+};
+
+let minus = document.getElementById("minus");
+minus.addEventListener("click", minusBtn);
+function minusBtn() {
+  document.getElementById("results").value += "-";
+};
+
+let multiply = document.getElementById("multiply");
+multiply.addEventListener("click", multiplyBtn);
+function multiplyBtn() {
+  document.getElementById("results").value += "*";
+};
+
+let division = document.getElementById("division");
+division.addEventListener("click", divisionBtn);
+function divisionBtn() {
+  document.getElementById("results").value += "/";
+};
+
+let one = document.getElementById("one");
+one.addEventListener("click", NumOne);
+function NumOne() {
+  document.getElementById("results").value += "1";
+};
+
+let two = document.getElementById("two");
+two.addEventListener("click", NumTwo);
+function NumTwo() {
+  document.getElementById("results").value += "2";
+};
+
+let three = document.getElementById("three");
+three.addEventListener("click", NumThree);
+function NumThree() {
+  document.getElementById("results").value += "3";
+};
+
+let four = document.getElementById("four");
+four.addEventListener("click", NumFour);
+function NumFour() {
+  document.getElementById("results").value += "4";
+};
+
+let five = document.getElementById("five");
+five.addEventListener("click", NumFive);
+function NumFive() {
+  document.getElementById("results").value += ("5");
+};
+
+let six = document.getElementById("six");
+six.addEventListener("click", NumSix);
+function NumSix() {
+  document.getElementById("results").value += "6";
+};
+
+let seven = document.getElementById("seven");
+seven.addEventListener("click", NumSeven);
+function NumSeven() {
+  document.getElementById("results").value += "7";
+};
+
+let eight = document.getElementById("eight");
+eight.addEventListener("click", NumEight);
+function NumEight() {
+  document.getElementById("results").value += "8";
+};
+
+let nine = document.getElementById("nine");
+nine.addEventListener("click", NumNine);
+function NumNine() {
+  document.getElementById("results").value += "9";
+};
+
+let zero = document.getElementById("zero");
+zero.addEventListener("click", NumZero);
+function NumZero() {
+  document.getElementById("results").value += "0";
+};
+
+let ppint = document.getElementById("point");
+point.addEventListener("click", pointBtn);
+function pointBtn() {
+  document.getElementById("results").value += ".";
+};
